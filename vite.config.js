c5fed7339db586465932b9e1647b5d0f5fdf26b4
unknown <dasharathgarai1@gmail.com>
2025-06-19 00:45:02 -0700

    add

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // 🛠️ Default but good to make it explicit
+  },
+  base: '/', // ✅ VERY important for correct route handling
 })
-
